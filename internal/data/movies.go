04879b93a4dcb12896809a39ad41ccd1404f29b3
alvinrw <alvin@mail.com>
2022-05-25 23:43:27 +0800

    chore: json formatting

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,10 +4,10 @@ import "time"
 
 type Movie struct {
 	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
-	Year      int32     `json:"year"`    // movie release year
-	Runtime   int32     `json:"runtime"` // movie runtime in minutes
-	Genres    []string  `json:"genres"`
+	Year      int32     `json:"year,omitempty"`    // movie release year
+	Runtime   int32     `json:"runtime,omitempty"` // movie runtime in minutes
+	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"` // starts at 1 and will be incremented each time the movie information is updated
 }
