026cae16b5148759864769110f78743df2123984
alvinrw <alvin@mail.com>
2022-05-26 00:08:18 +0800

    refactor: create custom runtime values and change MarshalJSON method

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -7,7 +7,7 @@ type Movie struct {
 	CreatedAt time.Time `json:"-"`
 	Title     string    `json:"title"`
 	Year      int32     `json:"year,omitempty"`    // movie release year
-	Runtime   int32     `json:"runtime,omitempty"` // movie runtime in minutes
+	Runtime   Runtime   `json:"runtime,omitempty"` // movie runtime in minutes
 	Genres    []string  `json:"genres,omitempty"`
 	Version   int32     `json:"version"` // starts at 1 and will be incremented each time the movie information is updated
 }
